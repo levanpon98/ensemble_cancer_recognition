c5ec1a71ad4bd062ce6c87f614f7864fe435bbcd
levanpon98 <levanpon98@gmail.com>
2020-05-05 22:52:45 +0700

    Init source

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 image_width = 128
 image_height = 128
 min_cases = 1000
-data_path = '/home/levanpon/data/ChestXray-NIHCC/'
+data_path = '../x-ray-data'
 model_name = 'densenet'
 model_path = 'saved'
